e0f9c68a90bd267b2ad12cc2cb4dd0a5a27c387a
Nacho <nacabaro@debian.localdomain>
2023-10-08 18:37:04 +0200

    Fixed issue with the Monitor class

--- src/es/uco/summercampmanager/Monitor.java
+++ src/es/uco/summercampmanager/Monitor.java
@@ -1,23 +1,19 @@
 package es.uco.summercampmanager;
 
 public class Monitor extends Person {
-	private String dateOfBirth_;
-	private Boolean specialNeeds_;
+	private Boolean specialNeedsEducator_;
 	
-	public Boolean getSpecialNeeds() { return specialNeeds_; }
-	public String getDateOfBirth() { return dateOfBirth_; }
-	public void setSpecialNeeds(Boolean specialNeeds) { specialNeeds_ = specialNeeds; }
-	public void setDateOfBirth(String dateOfBirth) { dateOfBirth_ = dateOfBirth; }
+	public Boolean getSpecialNeedsEducator() { return specialNeedsEducator_; }
+	public void setSpecialNeedsEducator(Boolean specialNeeds) { specialNeedsEducator_ = specialNeeds; }
 	
 	@Override
 	public String toString() {
-		return "ID: " + getId() + "\nNombre: " + getNombre() + "\nApellidos: " + getApellidos() + "\nSpecial needs: " + specialNeeds_ + "\nDate of Birth: " + dateOfBirth_;
+		return "ID: " + getId() + "\nNombre: " + getNombre() + "\nApellidos: " + getApellidos() + "\nSpecial needs: " + specialNeedsEducator_ + "\n";
 	}
 	
 	public Monitor() {}
-	public Monitor(int id, String nombre, String apellidos, String dateOfBirth, Boolean specialNeeds) {
+	public Monitor(int id, String nombre, String apellidos, Boolean specialNeeds) {
 		super(id, nombre, apellidos);
-		setSpecialNeeds(specialNeeds);
-		setDateOfBirth(dateOfBirth);
+		setSpecialNeedsEducator(specialNeeds);
 	}
 }
