ffcc1398a0c2d6399468a8262d13191ff9c1e466
Torres2404 <jandres.tm2002@gmail.com>
2023-10-09 15:01:03 +0200

    Update ClassCamp.java

--- src/ClassCamp.java
+++ src/ClassCamp.java
@@ -3,32 +3,32 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-public class ClassCamp extends ClassMonitor,ClassActivity{
+public class ClassCamp extends ClassMonitor{
     private int idCamp;
     private Date beginningDate;
     private Date endingDate;
-    private String educativeLevel;
+    private EnumLevel level_;
     private int maxAssistants;
-    private List<activity> activity;
-    private Monitor Monitor;
-    private Monitor specialMonitor;
+    private List<ClassActivity> activity;
+    private ClassMonitor[] monitors_;
+    private int minMonitors;
+    private ClassMonitor responsibleMonitor;
+    private ClassMonitor responsiblespecialMonitor;
 
-    
     public ClassCamp() {
-        activity = new ArrayList<>();
+        activity = new ArrayList<ClassActivity>() ;
     } 
-    public Camp(int idCamp, Date beginningDate, Date endingDate, String educativeLevel, int maxAssistants) {
+    public ClassCamp(int idCamp, Date beginningDate, Date endingDate, EnumLevel level, int maxAssistants) {
         this.idCamp = idCamp;
         this.beginningDate = beginningDate;
         this.endingDate = endingDate;
-        this.educativeLevel = educativeLevel;
+        this.level_ = level;
         this.maxAssistants = maxAssistants;
-        activity = new ArrayList<>();
+        activity = new ArrayList<ClassActivity>();
     }
 
     
-    public int getIdCamp() {return idCamp;
-    }
+    public int getIdCamp() {return idCamp;}
 
     public void setIdCamp(int idCamp) {this.idCamp = idCamp;}
 
@@ -40,37 +40,49 @@ public class ClassCamp extends ClassMonitor,ClassActivity{
 
     public void setendingDate(Date endingDate) {this.endingDate = endingDate;    }
 
-    public String geteducativeLevel() {return educativeLevel; }
+    public EnumLevel geteducativeLevel() {return level_;}
 
-    public void seteducativeLevel(String educativeLevel) {this.educativeLevel = educativeLevel;}
+    public void seteducativeLevel(EnumLevel educativeLevel) {this.level_ = educativeLevel;}
 
     public int getmaxAssistants() {return maxAssistants;}
 
     public void setmaxAssistants(int maxAssistants) {this.maxAssistants = maxAssistants;}
 
-    public List<activity> getactivityes() {return activity;}
+    public List<ClassActivity> getClassactivity() {return activity;}
 
-    public void setactivityes(List<activity> activity) {this.activity = activity;}
+    public void setClassactivity(List<ClassActivity> activity) {this.activity = activity;}
 
-    public Monitor getMonitor() {return Monitor;}
+    public ClassMonitor[] getMonitors() {return monitors_;}
 
-    public void setMonitor(Monitor Monitor) {this.Monitor = Monitor;}
+    public int getminMonitors() {return minMonitors;}
 
-    public void associateActivity(Activity activity) {
-        if (activity.geteducativeLevel().equals(this.educativeLevel)) {
-            activity.add(activity);
+    public void setminMonitors(int minMonitors) {this.minMonitors = minMonitors;}
+
+    public ClassMonitor getresponsibleMonitor() {return responsibleMonitor;}
+
+    public void setresponsibleMonitors(ClassMonitor responsibleMonitor) {this.responsibleMonitor = responsibleMonitor;}
+
+    public ClassMonitor getresponsiblespecialMonitor() {return responsiblespecialMonitor;}
+
+    public void setresponsiblespecialMonitor(ClassMonitor responsiblespecialMonitor) {this.responsiblespecialMonitor=responsiblespecialMonitor;}
+
+    public void setMonitor(ClassMonitor[] monitor) {this.monitors_ = monitor;}
+
+    public void associateActivity(ClassActivity activity) {
+        if (activity.getLevel_().equals(this.level_)) {
+            this.activity.add(activity);
         }
     }
 
-    public void associateSpecial(Monitor monitor) {
-        if (activity.contains(monitor.getactivity())) {
-            Monitor = monitor;
-        }
+    public void associateMonitor(ClassMonitor monitor) {
+        
+            responsibleMonitor=monitor;
+        
     }
 
-    public void associateSpecialMonitor(Monitor monitor) {
-        if (!activity.contains(monitor.getactivity())) {
-            specialMonitor = monitor;
+    public void associateSpecialMonitor(ClassMonitor monitor) {
+        if (monitor.getSpecialNeedsEducator()) {
+            responsiblespecialMonitor = monitor;
         }
     }
 
@@ -79,10 +91,10 @@ public class ClassCamp extends ClassMonitor,ClassActivity{
         return "Camp ID: " + idCamp +
                 "\nBeginning on: " + beginningDate +
                 "\nEnding on: " + endingDate +
-                "\nEducative level: " + educativeLevel +
+                "\nEducative level: " + level_ +
                 "\nMax number of assistants: " + maxAssistants +
                 "\nActivites: " + activity +
-                "\nResponsible monitor: " + Monitor +
-                "\nSpecial monitor: " + specialMonitor;
+                "\nResponsible monitor: " + responsibleMonitor +
+                "\nSpecial monitor: " + responsiblespecialMonitor;
     }
 }
