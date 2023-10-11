39f846725b86b37a32d7f8f4fc0d1e7e6b5739b1
naibu3 <pablipoyo@gmail.com>
2023-10-11 11:33:21 +0200

    Created ClassRegistration methods

--- src/ClassRegistration.java
+++ src/ClassRegistration.java
@@ -10,10 +10,10 @@ public class ClassRegistration {
     private EnumRegistrationType type_;
 
 
-    public int geyIdParticipant_() {
+    public int getIdParticipant() {
         return idPartcipant_;
     }
-    public int geyIdCamp_() {
+    public int getIdCamp() {
         return idCamp_;
     }
     public LocalDate getRegistrationDate() {
@@ -22,7 +22,32 @@ public class ClassRegistration {
     public float getPrice() {
         return price_;
     }
-    public EnumRegistrationType geyType_() {
+    public EnumRegistrationType getType() {
         return type_;
+    }
+
+    public void setIdParticipant_(int id) {
+        idPartcipant_=id;
+    }
+    public void setIdCamp_(int id) {
+        idCamp_=id;
+    }
+    public void setRegistrationDate(LocalDate date) {
+        RegistrationDate_ = date;
+    }
+    public void setPrice(float price) {
+        price_=price;
+    }
+    public void setType(EnumRegistrationType type) {
+        type_=type;
+    }
+
+    public String toString(){
+        String string = "IdParticipant:"+getIdParticipant()+
+                        "\nIdCamp:"+getIdCamp()+
+                        "\nRegistrationDate:"+getRegistrationDate()+
+                        "\nPrice:"+getPrice()+
+                        "\nType:"+getType()+"\n";
+        return string;
     }
 }   
