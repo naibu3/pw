4f277e54b2caae7646e8adb5d2ca6057e67acd02
naibu3 <pablipoyo@gmail.com>
2023-10-12 18:45:48 +0200

    Created ClassRegistrationFactory

---
+++ src/ClassRegistrationFactory.java
@@ -0,0 +1,21 @@
+public class ClassRegistrationFactory {
+
+    public ClassRegistration createEarlyRegistration(){
+
+        /*The registration is made at least 15 days in advance, and allows 
+        the possibility to cancel it, regardless of whether it is full or partial.*/
+        
+        ClassRegistration registration = new ClassRegistration();
+		return registration;
+    }
+    
+    public ClassRegistration createLateRegistration(){
+
+        /*Registration is made less than 15 days in advance and at least 48 
+        hours  before  the  start  of  the  camp.  It  does  not  allow  for  cancellation,  and  is 
+        applicable to both full and partial registrations.*/
+
+        ClassRegistration registration = new ClassRegistration();
+		return registration;
+    }
+}
