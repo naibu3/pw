ba22e2436fefeba7e8206dd5b5260d4d22775297
Ángela González Fontiveros <116628814+im4ngy@users.noreply.github.com>
2023-11-09 19:13:40 +0100

    Update RegistrationDAO.java

--- src/data/dao/RegistrationDAO.java
+++ src/data/dao/RegistrationDAO.java
@@ -1,15 +1,30 @@
 package data.dao;
 
-import java.util.Properties;
-import java.util.Hashtable;
-import java.io.FileInputStream;
-import java.time.LocalDate;
-import java.util.ArrayList;
-
-import data.dto.RegistrationDTO;
 import data.common.DBConnection;
+import data.dto.RegistrationDTO;
 
-public class RegistrationDAO{
-    
-    
+public class RegistrationDAO {
+    public RegistrationDAO(){}
+
+    /**
+	 * Creates a new registration
+	 * @param registration
+	 * @return true on success
+	 */
+	public boolean createRegistration(RegistrationDTO registration) {
+		int status = -1;
+		try {
+			DBConnection dbConnection = new DBConnection();
+			dbConnection.getConnection();
+
+			status = dbConnection.createRegistration(registration.getIdParticipant(), registration.getIdCamp(), registration.getRegistrationDate(), registration.getPrice(), "full");
+			
+			dbConnection.closeConnection();
+			
+		} catch (Exception e){
+			System.err.println(e);
+			e.printStackTrace();
+		}
+		return (status == 1);
+	}
 }
